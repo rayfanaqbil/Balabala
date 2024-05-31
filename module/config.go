701c21566f5709f1c6900b2412ab44d97a8749ec
rayfanaqbil <rayfana09@gmail.com>
2024-05-31 14:49:15 +0700

    change file

---
+++ module/config.go
@@ -0,0 +1,15 @@
+package _714220044
+
+import (
+	"github.com/aiteung/atdb"
+	"os"
+)
+
+var MongoString string = os.Getenv("MONGOSTRING")
+
+var MongoInfo = atdb.DBInfo{
+	DBString: MongoString,
+	DBName:   "tesdb2024",
+}
+
+var MongoConn = atdb.MongoConnect(MongoInfo)
